d06fa7c4f0eab07d16de119280ff7442ca78e745
垚晖 <50758821+Jinyinghui@users.noreply.github.com>
2026-02-28 16:41:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   admin: {
-    username: process.env.ADMIN_USERNAME || 'admin',
-    password: process.env.ADMIN_PASSWORD || '123456'
+    username: process.env.ADMIN_USERNAME || 'yaohuiadmin',
+    password: process.env.ADMIN_PASSWORD || '553423889@qq.cn'
   },
   server: {
     port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || 'nav-item-jwt-secret-2024-secure-key'
+    jwtSecret: process.env.JWT_SECRET || 'RhDrJZappF9o4x4GPB7S+R7Hge8UVY/Ko+IPYNtuHVw='
   }
+
 }; 
